2721f4fe78cd6d590c0d057704fe6f86fb2792d9
TienDat <tadat290903@gmail.com>
2024-07-18 19:20:09 +0700

    update project structure (6th)

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+    include "./config/database.php";
+    include "./app/models/index.php";
+    include "./app/repository/index.php";
+    include "./app/services/index.php";
+    include "./route/index.php";
+
+?>
---
+++ route/site.php
@@ -0,0 +1,34 @@
+<?php
+    include "./app/controllers/SiteController.php";
+    include "./app/middlewares/SiteMiddleWare.php";
+    $siteMiddleWare = new SiteMiddleWare();
+    $siteController = new SiteController();
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    if(isset($_GET['param_1']) && isset($_GET['param_2']) && isset($_GET['param_3'])){
+
+    } else if (isset($_GET['param_1']) && isset($_GET['param_2'])){
+
+    } else if (isset($_GET['param_1'])){
+        switch ($_GET['param_1']){
+            case 'index.php':
+                $siteMiddleWare->index();
+                break;
+        }
+    }
+
+} else if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if(isset($_GET['param_1']) && isset($_GET['param_2']) && isset($_GET['param_3'])){
+
+    } else if (isset($_GET['param_1']) && isset($_GET['param_2'])){
+
+    } else if (isset($_GET['param_1'])){
+        switch ($_GET['param_1']){
+            case 'student':
+                $siteController->index_POST();
+                break;
+        }
+    }
+}
+
+?>
