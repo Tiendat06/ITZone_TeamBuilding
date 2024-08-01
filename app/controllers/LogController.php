4165e1e823858f279d97daa59442a3c80b3fa0c3
TienDat <tadat290903@gmail.com>
2024-08-02 01:14:43 +0700

    push pseudo code for login

---
+++ app/controllers/LogController.php
@@ -0,0 +1,19 @@
+<?php
+
+class LogController{
+    public function __construct()
+    {
+    }
+
+//    [GET] /log/login
+    public function index($username, $pwd){
+
+    }
+
+//    [POST]
+    public function checkLogin($username, $pwd){
+
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
     include "./config/DatabaseManager.php";
-//    include "./config/database.php";
     include "./app/models/index.php";
     include "./app/pattern/index.php";
     include "./app/repository/index.php";
