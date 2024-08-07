5f775563366b23fb46c67e70f3707909f6ad5148
TienDat <tadat290903@gmail.com>
2024-08-08 01:27:56 +0700

    update guard main page (check location)

--- views/guard/guard.php
+++ views/guard/guard.php
@@ -1,2 +1,26 @@
 <?php
 ?>
+
+
+<div class="container">
+    <div class="row guard">
+        <div class="guard-sub-title d-flex flex-wrap align-items-center text-center">
+            <div class="guard-sub-title__line col-sm-3"></div>
+            <span class="guard-sub-title__para col-sm-6">Chào mừng đến trạm</span>
+            <div class="guard-sub-title__line col-sm-3"></div>
+        </div>
+
+        <div class="guard-title col-sm-12 text-center mt-2">
+            <h1 class="guard-title__para itz-btn-hover">Công viên 30/4</h1>
+        </div>
+
+        <div class="guard-location">
+            <iframe class="guard-location__ggmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.4199643219035!2d106.6973647!3d10.7791119!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f6570c90b25%3A0x788f1a06c37e1848!2sApril%2030th%20Park!5e0!3m2!1sen!2s!4v1723054211156!5m2!1sen!2s" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+
+        <a href="#" class="guard-address text-center">
+            <i class="fa-solid fa-location-dot guard-address__icon"></i>
+            <span class="guard-address__para">6 Đ. Lê Duẩn, Bến Nghé, Quận 1, Hồ Chí Minh, Vietnam</span>
+        </a>
+    </div>
+</div>
