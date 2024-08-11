0d83f00f238020531710324510bc0bdaf8563c35
TienDat <tadat290903@gmail.com>
2024-08-12 02:12:12 +0700

    update mentor topic page

--- views/team/mentor_topic.php
+++ views/team/mentor_topic.php
@@ -1,2 +1,35 @@
 <?php
 ?>
+
+<div class="container">
+    <div class="row">
+        <div class="team-mentor_topic">
+            <div class="team-mentor_topic__header d-flex flex-wrap">
+                <div class="team-mentor_topic__line col-sm-3 col-md-3"></div>
+                <span class="team-mentor_topic__header-title text-center col-sm-6 col-md-6">Hãy tìm mentor của bạn</span>
+                <div class="team-mentor_topic__line col-sm-3 col-md-3"></div>
+            </div>
+
+            <div class="team-mentor_topic__sub-header">
+                <h1 class="team-mentor-topic__sub-header-title">Mật thư</h1>
+                <span class="team-mentor_topic__sb-header-sub-title">Giải mật thư tìm Mentor của bạn.</span>
+            </div>
+
+            <div class="team-mentor_topic__content d-flex flex-wrap">
+                <div class="team-mentor_topic__logo col-sm-12 col-md-12 text-center">
+                    <img style="width: 200px" src="/public/img/topic/mail-img.png" alt="">
+                </div>
+                <a href="#" class="team-mentor_topic__link"> Mật thư truy tìm Mentor </a>
+                <span class="team-mentor_topic__text text-center col-sm-12 col-md-12">
+                    Chào mừng bạn đến mật thư tìm Mentor, hãy nhanh chóng tìm ra đáp án đến với Mentor của bạn
+                </span>
+                <p class="team-mentor_topic__time">29 : 59</p>
+
+                <div class="form-group col-sm-12 col-md-12 d-flex flex-wrap justify-content-between">
+                    <input placeholder="Nhập đáp án" type="text" class="col-sm-8 col-md-8 form-control team-mentor_topic__inp">
+                    <button type="submit" class="col-sm-3 col-md-3 itz-bg-normal team-mentor_topic__btn">Gửi</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
