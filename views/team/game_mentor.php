c7f69f118f8eebfe993a05a271334454c8920056
TienDat <tadat290903@gmail.com>
2024-08-12 01:25:35 +0700

    update game_mentor page

--- views/team/game_mentor.php
+++ views/team/game_mentor.php
@@ -1,2 +1,32 @@
 <?php
 ?>
+
+<div class="container">
+    <div class="row">
+        <div class="team-game_mentor">
+            <div class="team-game_mentor__header">
+                <img class="w-100" src="/public/img/team/game_mentor_home.png" alt="">
+                <h1 class="text-center team-game_mentor__title">Chào mừng Gen 8</h1>
+            </div>
+
+            <div class="team-game_mentor__content d-flex flex-wrap justify-content-center">
+                <p class="team-game_mentor__para col-sm-12 col-md-12" style="width: 90%">
+                    “Đến với hành trình team building đầy thử thách!
+                    Hãy cùng đồng đội vượt qua mọi giới hạn
+                    và chinh phục chiến thắng trong cuộc đua này.
+                    Cùng nhau tạo nên những kỷ niệm không thể quên!
+                    Mentor đang chờ bạn! Đừng chần chừ nữa.”
+
+                </p>
+                <div class="team-game_mentor__btn col-sm-12 col-md-12 text-center">
+                    <a href="#" class="itz-bg-normal team-game_mentor__btn-go">
+                        <span>
+                            Bắt đầu ngay thôi !!
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
