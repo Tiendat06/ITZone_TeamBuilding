d41856a911666e6351e9f62d34f850d577ce9185
TienDat <tadat290903@gmail.com>
2024-09-20 15:39:31 +0700

    fix maintenance page

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
     session_set_cookie_params(86400);   // 1 day
     session_start();
-    $maintain = true;
     include "./config/DatabaseManager.php";
     include "./app/models/index.php";
     include "./app/pattern/index.php";
     include "./app/repository/index.php";
     include "./app/services/index.php";
-    if($maintain)
-        include "./views/error/maintain.php";
-    else
-        include "./route/index.php";
+    include "./route/index.php";
 ?>
--- route/index.php
+++ route/index.php
@@ -1,9 +1,14 @@
 <?php
-    include "./route/site.php";
-    include "./route/log.php";
-    include "./route/mentor.php";
-    include "./route/guard.php";
-    include "./route/support.php";
-    include "./route/team.php";
-    include "./route/location.php";
+    $maintain = false;
+    if($maintain)
+        include "./views/error/maintain.php";
+    else{
+        include "./route/site.php";
+        include "./route/log.php";
+        include "./route/mentor.php";
+        include "./route/guard.php";
+        include "./route/support.php";
+        include "./route/team.php";
+        include "./route/location.php";
+    }
 ?>
