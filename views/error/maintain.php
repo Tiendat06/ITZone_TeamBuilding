fcaf4580160fd5155fce9e4484d3357a5302af9b
TienDat <tadat290903@gmail.com>
2024-09-20 14:11:37 +0700

    update maintenance page

---
+++ views/error/maintain.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <?php
+        include "./views/layout/partials/header.php";
+    ?>
+</head>
+<body style="background-color: #fcfcfc; height: 100vh">
+
+    <div class="text-center h-100 pt-5">
+        <img style="width: 100%" src="/public/img/error/maintenance_2.gif" alt="">
+        <h1 style="font-weight: bold">Vui lòng quay lại sau</h1>
+        <p>Trang web đang được bảo trì</p>
+    </div>
+
+</body>
+</html>
