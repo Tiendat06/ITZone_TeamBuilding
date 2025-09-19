89c39295478fcab19c29bf4f18067092561d81e0
BDragon <baolong2k6iter@gmail.com>
2025-09-19 19:40:46 +0700

    add pop up button

--- views/team/team_game_1.php
+++ views/team/team_game_1.php
@@ -1,14 +1,15 @@
 <?php
-    /**
-     * @var $team_arrival_location
-     */
+
+/**
+ * @var $team_arrival_location
+ */
 ?>
 
 
 <div class="team">
     <div class="team-intro d-flex flex-wrap">
         <div class="team-intro__title col-sm-12">
-            <h1 class="team-intro__header">Mật thư trạm</h1>
+            <h1 class="team-intro__header">Mật thư trạm </h1>
             <img style="" class="w-100 team-intro__img" src="/public/img/team/intro.png" alt="">
         </div>
         <div class="team-intro__sub-title d-flex flex-wrap">
@@ -18,47 +19,73 @@
         </div>
     </div>
 
-    <div class="team-content d-flex">
-        <?php
-            $index = 0;
-            $bus_back = '';
-            foreach ($team_arrival_location as $row){
-                $location_id = $row['location_id'];
-                $is_show_next_location = $row['is_show_next_location'];
-                $location_img = $row['location_img'];
-                $team_arrival_priority = $row['team_arrival_priority'];
-                $url_link = '/team/game-1-topic/'.$location_id;
-                $disable_card = '';
-                $previous_location = '';
-                if($is_show_next_location == 0){
-                    $disable_card = 'itz-disable-card';
-                    $url_link = '#';
-                }
-                if($index == 3) $bus_back = $row['bus_back'];
-                if($location_id === 'LOC0000006') $url_link = '#';
-        ?>
-        <a <?= $location_id == 'LOC0000006' && $is_show_next_location == 1?
-            'data-bs-target="#team-complete" data-bs-toggle="modal" data-back="'.$bus_back.'" data-location="'.$location_id.'"'
-            : '' ?>
-                href="<?=$url_link?>" class="team-content__item <?= $is_show_next_location == 1 && $location_id != 'LOC0000006'? 'loading-item': '' ?> d-block <?=$disable_card?> col-sm-4 col-md-4">
-            <div class="team-content__item--inner">
-                <div class="team-content__item--info text-center">
-                    <div class="team-content__item--img">
-                        <img style="width: 95px; height: 100px" src="/public/img/topic/<?=$location_img?>" alt="">
+    <div class="team-letter">
+        <!-- Button trigger modal -->
+        <button type="button" class="team-letter__button" data-bs-toggle="modal" data-bs-target="#exampleModal" style="right:10px; top:-10px">
+            <img src="../../public/img/topic/icon-special-letter.jpg" class="team-letter__img" alt="" >
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade team-letter__content" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div class="team-content__item--header">
-                        <h1 class="team-content__item--title mb-0">Trạm <?=$team_arrival_priority - 1?></h1>
+                    <div class="modal-body">
+                        ...
                     </div>
-                    <div class="team-content__item--play">
-                        <img src="/public/img/icon/icon-play.png" alt="">
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
                     </div>
                 </div>
             </div>
-        </a>
+        </div>
+    </div>  
+
+    <div class="team-content d-flex">
+        <?php
+        $index = 0;
+        $bus_back = '';
+        foreach ($team_arrival_location as $row) {
+            $location_id = $row['location_id'];
+            $is_show_next_location = $row['is_show_next_location'];
+            $location_img = $row['location_img'];
+            $team_arrival_priority = $row['team_arrival_priority'];
+            $url_link = '/team/game-1-topic/' . $location_id;
+            $disable_card = '';
+            $previous_location = '';
+            if ($is_show_next_location == 0) {
+                $disable_card = 'itz-disable-card';
+                $url_link = '#';
+            }
+            if ($index == 3) $bus_back = $row['bus_back'];
+            if ($location_id === 'LOC0000006') $url_link = '#';
+        ?>
+            <a <?= $location_id == 'LOC0000006' && $is_show_next_location == 1 ?
+                    'data-bs-target="#team-complete" data-bs-toggle="modal" data-back="' . $bus_back . '" data-location="' . $location_id . '"'
+                    : '' ?>
+                href="<?= $url_link ?>" class="team-content__item <?= $is_show_next_location == 1 && $location_id != 'LOC0000006' ? 'loading-item' : '' ?> d-block <?= $disable_card ?> col-sm-4 col-md-4">
+                <div class="team-content__item--inner">
+                    <div class="team-content__item--info text-center">
+                        <div class="team-content__item--img">
+                            <img style="width: 95px; height: 100px" src="/public/img/topic/<?= $location_img ?>" alt="">
+                        </div>
+                        <div class="team-content__item--header">
+                            <h1 class="team-content__item--title mb-0">Trạm <?= $team_arrival_priority - 1 ?></h1>
+                        </div>
+                        <div class="team-content__item--play">
+                            <img src="/public/img/icon/icon-play.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </a>
 
         <?php
-                $index++;
-            }
+            $index++;
+        }
         ?>
 
     </div>
@@ -73,7 +100,7 @@
                 </div>
                 <div class="team-game__para p-3">
                     <span id="itz-modal-text-end" style="font-size: 16px; font-weight: bold;-webkit-text-fill-color: transparent;-webkit-background-clip: text;"
-                          class="itz-btn-home">
+                        class="itz-btn-home">
                         Vui lòng chờ gợi ý vài giây
                     </span>
                 </div>
@@ -87,7 +114,7 @@
 
 <script>
     window.addEventListener('DOMContentLoaded', () => {
-        if(window.getBusBack){
+        if (window.getBusBack) {
             getBusBack();
         }
     })
