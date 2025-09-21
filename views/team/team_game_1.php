9c68ae468fbf2287f5b72805133599ef8e9af9c2
HuynhTrungTin52 <524k0033@student.tdtu.edu.vn>
2025-09-21 19:00:30 +0700

    FIX EVERYTHING
    PULL REQUEST RUINED EVERYTHING AND THINGS SHOULD BE FIXED NOW

--- views/team/team_game_1.php
+++ views/team/team_game_1.php
@@ -20,10 +20,7 @@
     </div>
     <div class="team-letter">
         <!-- Button trigger modal -->
-        <!-- <div class="team-letter__button">
-    
-        </div> -->
-        <button type="button" class="btn itz-btn-popup team-letter__button" data-bs-toggle="modal" data-bs-target="#exampleModal"></button>
+        <button type="button" class="itz-btn-trigger team-letter__trigger" data-bs-toggle="modal" data-bs-target="#exampleModal"></button>
         <!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class=" modal-dialog modal-dialog-centered">
@@ -36,11 +33,11 @@
                     <div class="modal-body team-letter__content">
                         Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
                     </div>
-                    <div class="modal-footer" style="border:none;">
-                        <div class="form-group col-sm-12 col-md-12 d-flex flex-wrap justify-content-between">
+                    <div class="team-letter__footer">
+                        <div class="form-group d-flex justify-content-between">
                             <!-- Make your own button -->
-                            <input id="team--letter-input" placeholder="Nhập đáp án" type="text" class="rounded col-sm-8 col-md-8 form-control team-letter__inp">
-                            <button id="team--letter-btn" type="submit" class="col-sm-3 col-md-3 team-letter__btn rounded">Gửi</button>
+                            <input id="team--letter-input" placeholder="Nhập đáp án" type="text" class="rounded col-sm-8 col-md-8  team-letter__inp">
+                            <button id="team--letter-btn" type="submit" class="rounded col-sm-3 col-md-3 team-letter__btn">Gửi</button>
                         </div>
                     </div>
                 </div>
