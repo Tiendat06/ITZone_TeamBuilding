3d3a81f27de8dc9e862c13a05d0845f4aa5119f3
BDragon <baolong2k6iter@gmail.com>
2025-09-24 14:06:04 +0700

    Update DatabaseManager.php

--- config/DatabaseManager.php
+++ config/DatabaseManager.php
@@ -4,22 +4,28 @@
 //    define("password", "root");
 //    define("database", "teambuilding");
 
-class DatabaseManager{
+class DatabaseManager
+{
+    // private string $hostname = "db";
+    // private string $username = "user";
+    // private string $password = "MyPassword123.";
     private string $hostname = "localhost";
     private string $username = "root";
     private string $password = "";
     private string $database = "teambuilding";
     private static ?DatabaseManager $instance = null;
     private ?mysqli $conn = null;
-    private function __construct(){}
+    private function __construct() {}
 
-    public static function getInstance(): DatabaseManager{
-        if(self::$instance === null)
+    public static function getInstance(): DatabaseManager
+    {
+        if (self::$instance === null)
             self::$instance = new DatabaseManager();
         return self::$instance;
     }
 
-    public function getConnection(): mysqli {
+    public function getConnection(): mysqli
+    {
         if ($this->conn === null) {
             $this->conn = mysqli_connect($this->hostname, $this->username, $this->password, $this->database);
             if (!$this->conn) {
@@ -30,7 +36,8 @@ class DatabaseManager{
         return $this->conn;
     }
 
-    public function closeConnection(): void {
+    public function closeConnection(): void
+    {
         if ($this->conn !== null) {
             mysqli_close($this->conn);
             $this->conn = null;
@@ -38,17 +45,15 @@ class DatabaseManager{
         self::$instance = null;
     }
 
-//    public function getConnection(): mysqli{
-//        $this->conn = mysqli_connect(hostname, username, password, database);
-//        if(!$this->conn)
-//            die("".mysqli_connect_error());
-//        return $this->conn;
-//    }
-//
-//    public function closeConnection(): void{
-//        self::$instance = null;
-//        mysqli_close($this->conn);
-//    }
+    //    public function getConnection(): mysqli{
+    //        $this->conn = mysqli_connect(hostname, username, password, database);
+    //        if(!$this->conn)
+    //            die("".mysqli_connect_error());
+    //        return $this->conn;
+    //    }
+    //
+    //    public function closeConnection(): void{
+    //        self::$instance = null;
+    //        mysqli_close($this->conn);
+    //    }
 }
-
-?>
